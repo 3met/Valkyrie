c24e8fc1268d8cb4e7b233f4d4b81cea1f1ea547
3met <47290143+3met@users.noreply.github.com>
2021-06-03 17:14:25 -0400

    Add "perft test" command to UCI

--- src/uci_functionality/input_perft.cpp
+++ src/uci_functionality/input_perft.cpp
@@ -1,12 +1,35 @@
 
+#include <chrono>
 #include <string>
 #include <vector>
+#include "testing.hpp"
 #include "uci.hpp"
 
+using namespace std::chrono;
+
+/* For UCI commands that start with "perft" */
 void UCI::inputPerft(string input) {
 
+	// Parse input
 	std::vector<string> inVec;
 	this->splitString(input, &inVec);
 
-	cout << this->engine.perft(&cs, stoi(inVec[1])) << endl;
+	if (inVec[1] == "test") {
+
+		auto start = high_resolution_clock::now();
+
+		bool passed = perftTest();
+
+		if (passed) {
+			cout << "PASSED" << endl;
+		} else {
+			cout << "FAILED" << endl;
+		}
+
+		auto stop = high_resolution_clock::now();
+		auto duration = duration_cast<microseconds>(stop - start);
+		cout << "Duration: " << duration.count() << endl;
+	} else {
+		cout << this->engine.perft(&cs, stoi(inVec[1])) << endl;
+	}
 }
