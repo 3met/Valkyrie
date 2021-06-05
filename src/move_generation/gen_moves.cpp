472baf04cbbea41819d72787cfb72c375dc05ec6
3met <47290143+3met@users.noreply.github.com>
2021-06-04 20:23:28 -0400

    Add filter to ensure pieces exists before genAllMoves()

--- src/move_generation/gen_moves.cpp
+++ src/move_generation/gen_moves.cpp
@@ -5,10 +5,20 @@
 void ChessEngine::genAllMoves(ChessState* cs, vector<Move>* moves) {
 	/* Generates all moves in a givin ChessState */
 
-	genPMoves(cs, moves);
-	genNMoves(cs, moves);
-	genBMoves(cs, moves);
-	genRMoves(cs, moves);
-	genQMoves(cs, moves);
+	if (cs->pieces[cs->turn][cs->PAWN].board) {
+		genPMoves(cs, moves);
+	}
+	if (cs->pieces[cs->turn][cs->KNIGHT].board) {
+		genNMoves(cs, moves);
+	}
+	if (cs->pieces[cs->turn][cs->BISHOP].board) {
+		genBMoves(cs, moves);
+	}
+	if (cs->pieces[cs->turn][cs->ROOK].board) {
+		genRMoves(cs, moves);
+	}
+	if (cs->pieces[cs->turn][cs->QUEEN].board) {
+		genQMoves(cs, moves);
+	}
 	genKMoves(cs, moves);
 }
