bed68f70766ef2020c8362a17f9478759e776b31
3met <47290143+3met@users.noreply.github.com>
2021-06-08 14:51:23 -0400

    Add center bias in move ordering

--- include/move_compare.hpp
+++ include/move_compare.hpp
@@ -15,35 +15,12 @@ private:
 	ChessEngine* engine;
 	U8 depth;
 
+	static const U8 centerBias[64];
+
 public:
-	MoveCompare(ChessEngine* _engine, U8 _depth) {
-		engine = _engine;
-		depth = _depth;
-	};
+	MoveCompare(ChessEngine* _engine, U8 _depth);
 
-	bool operator()(const Move& a, const Move& b) const {
-		if (a.killed > b.killed) {
-			return true;
-		} else if (b.killed > a.killed) {
-			return false;
-		}
-
-		if (a.promoted > b.promoted) {
-			return true;
-		} else if (b.promoted > a.promoted) {
-			return false;
-		}
-
-		for (U8 i=0; i<engine->killerHeuristic[depth].size(); ++i) {
-			if (a == engine->killerHeuristic[depth][i]) {
-				return true;
-			} else if (b == engine->killerHeuristic[depth][i]) {
-				return false;
-			}
-		}
-
-		return a.start > b.start;	// Default
-	}
+	bool operator()(const Move& a, const Move& b) const;
 };
 
 #endif
---
+++ src/move_compare.cpp
@@ -0,0 +1,50 @@
+
+#include "move_compare.hpp"
+
+using namespace std;
+
+const U8 MoveCompare::centerBias[64] {
+	8, 7, 6, 5, 5, 6, 7, 8,
+	7, 5, 4, 3, 3, 4, 5, 7,
+	6, 4, 2, 1, 1, 2, 4, 6,
+	5, 3, 1, 0, 0, 1, 3, 5,
+	5, 3, 1, 0, 0, 1, 3, 5,
+	6, 4, 2, 1, 1, 2, 4, 6,
+	7, 5, 4, 3, 3, 4, 5, 7,
+	8, 7, 6, 5, 5, 6, 7, 8,
+};
+
+MoveCompare::MoveCompare(ChessEngine* _engine, U8 _depth) {
+	this->engine = _engine;
+	this->depth = _depth;
+};
+
+bool MoveCompare::operator()(const Move& a, const Move& b) const {
+	if (a.killed > b.killed) {
+		return true;
+	} else if (b.killed > a.killed) {
+		return false;
+	}
+
+	if (a.promoted > b.promoted) {
+		return true;
+	} else if (b.promoted > a.promoted) {
+		return false;
+	}
+
+	for (U8 i=0; i<engine->killerHeuristic[depth].size(); ++i) {
+		if (a == engine->killerHeuristic[depth][i]) {
+			return true;
+		} else if (b == engine->killerHeuristic[depth][i]) {
+			return false;
+		}
+	}
+
+	if (centerBias[a.end] < centerBias[b.end]) {
+		return true;
+	} else if (centerBias[b.end] < centerBias[a.end]) {
+		return false;
+	}
+
+	return false;	// Default
+}
