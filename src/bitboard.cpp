82a2beb85984a75bb33bbeace79e09cf1bede8e9
Emet Behrendt <47290143+3met@users.noreply.github.com>
2023-05-27 04:50:25 -0700

    Optimize Bitboard operations

--- src/bitboard.cpp
+++ src/bitboard.cpp
@@ -40,45 +40,44 @@ const U8 Bitboard::LOWEST_BIT_TABLE[256] = {
 
 // Set the given position to the given value
 void Bitboard::setPos(U8 pos, bool value) {
-	if (value) {
-		board ^= (-U64(1) ^ board) & (1ULL << pos);
-	} else {
-		board ^= (-U64(0) ^ board) & (1ULL << pos);
-	}
+	U64 mask = U64(1) << pos;
+	board = (board & ~mask) | (U64(value) << pos);
 }
 
-// Set the given position on (1)
+// Set the given position on (ie 1)
 void Bitboard::setPosOn(U8 pos) {
-	board ^= (-U64(1) ^ board) & (1ULL << pos);
+	board |= U64(1) << pos;
 }
 
-// Set the given position off (0)
+// Set the given position off (ie 0)
 void Bitboard::setPosOff(U8 pos) {
-	board ^= (-U64(0) ^ board) & (1ULL << pos);
+	board &= ~(U64(1) << pos);
 }
 
 // Returns the value of the bit position
 bool Bitboard::getPos(U8 pos) const {
-	return board >> pos & 1;
+	return (board >> pos) & 1;
 }
 
-// Returns the all positions with a positive value
+// Stores the positions of all 1s and returns the number of 1s found
 U8 Bitboard::getPosArr(U8 arr[]) const {
 	Bitboard bb(this->board);
 
 	U8 n(0);
-	for ( ; bb.board != 0; ++n) {
+	while (bb.board != 0) {
 		arr[n] = bb.popLSB();
+		++n;
 	}
 	return n;
 }
 
-// Returns the all positions with a positive value .
+// Stores the positions of all 1s and returns the number of 1s found
 // Resets board to zero.
 U8 Bitboard::popPosArr(U8 arr[]) {
 	U8 n(0);
-	for ( ; this->board != 0; ++n) {
+	while (this->board != 0) {
 		arr[n] = this->popLSB();
+		++n;
 	}
 	return n;
 }
@@ -88,27 +87,47 @@ U8 Bitboard::getFirstPos() {
 	return LSB();
 }
 
-// Pop lowest significant bit
-inline U8 Bitboard::LSB() {
-	U8* bytes = (U8*)(&this->board);
 
-	if (bytes[0]) {
-		return LOWEST_BIT_TABLE[bytes[0]];
-	} else if (bytes[1]) {
-		return LOWEST_BIT_TABLE[bytes[1]] + 8;
-	} else if (bytes[2]) {
-		return LOWEST_BIT_TABLE[bytes[2]] + 16;
-	} else if (bytes[3]) {
-		return LOWEST_BIT_TABLE[bytes[3]] + 24;
-	} else if (bytes[4]) {
-		return LOWEST_BIT_TABLE[bytes[4]] + 32;
-	} else if (bytes[5]) {
-		return LOWEST_BIT_TABLE[bytes[5]] + 40;
-	} else if (bytes[6]) {
-		return LOWEST_BIT_TABLE[bytes[6]] + 48;
-	} else {
-		return LOWEST_BIT_TABLE[bytes[7]] + 56;  
+// Wrapper function for LSB using intrinsics
+#if defined(__GNUC__) && (defined(__x86_64__) || defined(_M_X64))
+	#include <immintrin.h> // Include the header for intrinsics
+
+	inline U8 lsb_intrinsic(U64 board) {
+		//return _tzcnt_u64(board);
+		return __builtin_ctzll(board);
 	}
+#else
+	// Portable implementation for other compilers
+	inline U8 lsb_portable(U64 board) {
+		U8* bytes = (U8*)(board);
+
+		if (bytes[0]) {
+			return LOWEST_BIT_TABLE[bytes[0]];
+		} else if (bytes[1]) {
+			return LOWEST_BIT_TABLE[bytes[1]] + 8;
+		} else if (bytes[2]) {
+			return LOWEST_BIT_TABLE[bytes[2]] + 16;
+		} else if (bytes[3]) {
+			return LOWEST_BIT_TABLE[bytes[3]] + 24;
+		} else if (bytes[4]) {
+			return LOWEST_BIT_TABLE[bytes[4]] + 32;
+		} else if (bytes[5]) {
+			return LOWEST_BIT_TABLE[bytes[5]] + 40;
+		} else if (bytes[6]) {
+			return LOWEST_BIT_TABLE[bytes[6]] + 48;
+		} else {
+			return LOWEST_BIT_TABLE[bytes[7]] + 56;  
+		}
+	}
+#endif
+
+// Pop lowest significant bit using the fastest implementation
+inline U8 Bitboard::LSB() {
+#if defined(__GNUC__) && (defined(__x86_64__) || defined(_M_X64))
+	return lsb_intrinsic(this->board);
+#else
+	return lsb_portable(this->board);
+#endif
 }
 
 
